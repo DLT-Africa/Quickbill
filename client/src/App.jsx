d09b3ed74e0e025f8ba474f4c8a0e9b09cb4d890
Lameed88 <moradeyor88@gmail.com>
2023-12-18 21:33:59 +0100

    aboutPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
+import AboutPage from './pages/AboutPage'
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage/>} />
       </Routes>
     </>
   )
---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+import AboutUsHero from "../components/AboutUsHero";
+import AboutUsTeam from "../components/AboutUsTeam";
+import Footer from "../Components/Footer";
+
+const AboutPage = () => {
+  return (
+    <>
+      <NavBar />
+      <AboutUsHero />
+      <AboutUsTeam />
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
