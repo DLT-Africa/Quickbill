08e7cb41a800f1a2e5130d7dd4ea38ab9f0d9d48
Lameed88 <moradeyor88@gmail.com>
2023-12-19 00:06:49 +0100

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,44 @@
-import { Box, Flex, Text } from "@chakra-ui/react"
+import { Box, Flex, Text, Image } from "@chakra-ui/react"
 
 const Footer = () => {
   return (
     <>
-      <Flex as={'footer'} w={'full'} bg={'#C6D9FE'} justifyContent={'space-between'}>
-        <Flex flexDir={'column'} gap={10}>
-            <Text as={'h1'} fontSize={'2xl'}>HOW IT WORKS</Text>
+      <Flex as={'footer'} w={'full'} bg={'#C6D9FE'} justifyContent={'space-between'} px={20} py={20} >
+
+        <Flex flexDir={'column'} gap={10} borderBottom={'1px'}>
+            <Text as={'h1'} fontSize={'2xl'} fontWeight={700}>HOW IT WORKS</Text>
             <Box>
-              <Text as={'h1'} fontSize={'xl'}>HOW IT WORKS</Text>
-              <Text as={'h1'} fontSize={'xl'}>HOW IT WORKS</Text>
+              <Text as={'h1'} fontSize={'sm'} fontWeight={600}>Hire Employees</Text>
+              <Text as={'h1'} fontSize={'sm'} fontWeight={600}>Run Global Payroll</Text>
             </Box>
         </Flex>
+
+        <Flex flexDir={'column'} gap={10}>
+        <Text as={'h1'} fontSize={'2xl'} fontWeight={700}>SOLUTIONS</Text>
+        <Box>
+        <Text as={'h1'} fontSize={'sm'} fontWeight={600}>
+          Complience <br/> Payments <br/> For Finance Teams
+        </Text>
+        </Box>
+        </Flex>
+
+        <Flex flexDir={'column'} gap={10} >
+        <Text as={'h1'} fontSize={'2xl'} fontWeight={700}>RESOURCES</Text>
+        <Box>
+          <Text as={'h1'} fontSize={'sm'} fontWeight={600}>About</Text>
+        </Box>
+        </Flex>
+
+        <Flex flexDir={'column'} justifyContent={'center'} pb={50} >
+          <Image src='/QuickBill.png' width={40} height={10} />
+          <Flex   gap={5} pt={10}>
+          <Image src='/insta.png' width={10} height={10}/>
+          <Image src='/facebook.png' width={10} height={10}/>
+          <Image src='/link.png' width={10} height={10}/>
+          <Image src='/twitter.png'width={10} height={10} />
+        </Flex>
+          </Flex>
+
       </Flex>
     </>
   )
--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import NavBar from "../Components/NavBar";
 import AboutUsHero from "../components/AboutUsHero";
 import AboutUsTeam from "../components/AboutUsTeam";
-import Footer from "../Components/Footer";
+import Footer from "../components/Footer";
+
 
 const AboutPage = () => {
   return (
