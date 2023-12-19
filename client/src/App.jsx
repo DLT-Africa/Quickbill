b9d44712787f63110facc3ab8ac35d4c0641452a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-12-19 20:25:34 +0100

    first commit on login-signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
+import AuthPage from './pages/AuthPage'
+import LoginCard from './components/LoginCard'
+import SignUpCard from "./components/SignUpCard";
+
+
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        
+        <Route path='/auth' element={<AuthPage />} />
+        <Route path="/login" component={<LoginCard />} />
+        <Route path="/signup" component={<SignUpCard />} />
       </Routes>
     </>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <RecoilRoot>
+
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
@@ -13,4 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
+  </RecoilRoot>
 );
