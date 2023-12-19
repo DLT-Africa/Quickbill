21e3c80c6d7786d6395eb277f72db0e4f49293c4
DLT-Africa <142330919+DLT-Africa@users.noreply.github.com>
2023-12-19 19:12:44 +0100

    Revert "invoice and client pages"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
         <App />
-    
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
