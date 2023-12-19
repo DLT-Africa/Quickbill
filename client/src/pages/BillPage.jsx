33bf355ed64c9ccacb59fd49f11e6bd1d54d1bf7
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-19 15:45:43 +0100

    bill page

--- client/src/pages/BillPage.jsx
+++ client/src/pages/BillPage.jsx
@@ -1,12 +1,12 @@
 import { Box } from '@chakra-ui/react'
-import BillSec from '../components/BillSec'
+import BillCon from '../components/BillCon'
 import NavBar from '../Components/NavBar'
 
 const BillPage = () => {
   return (
     <Box as={'section'} bg={'#eee'}>
       <NavBar />
-      <BillSec />
+      <BillCon />
     </Box>
   )
 }
