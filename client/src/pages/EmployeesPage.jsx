36e781e35b28ba16dc20151a9b1ad8f4b5ec5207
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-12-20 16:54:39 +0100

    add employees page

---
+++ client/src/pages/EmployeesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Employees from "../components/Employees";
+
+const EmployeesPage = () => {
+  return (
+    <>
+      <Employees />
+    </>
+  );
+};
+
+export default EmployeesPage;
