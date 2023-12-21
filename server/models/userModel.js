45c0cc9e9893c68608e3925dfcdd40f2de1026aa
yusufroqib <yusufroqib@gmail.com>
2023-12-21 16:15:53 +0100

    Models

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,36 @@
-import mongoose from "mongoose";
+const mongoose = require ("mongoose");
 
 const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	phoneNumber: String,
+	businessName: String,
+	contactAddress: String,
+	paymentDetails: String,
+	employees: [
+		{
+			name: {
+				type: String,
+			},
+			email: {
+				type: String,
+			},
+			department: {
+				type: String,
+			},
+			jobTitle: {
+				type: String,
+			},
+		},
+	],
+	ownClients: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: "Client",
+		default: [],
+	},
 });
 
 const User = mongoose.model("User", userSchema);
 
-export default User;
+module.exports = User;
