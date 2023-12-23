8f0399ca5052380c7571ab90537a6bab865badea
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-23 21:27:12 +0100

    employee controller

--- server/controllers/invoiceController.js
+++ server/controllers/invoiceController.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const invoiceModel = require("../models/invoiceModel");
 
 const createInvoice = async (req, res) => {
+  try {
   const invoice = req.body;
 
   const newInvoice = new invoiceModel(invoice);
 
-  try {
+  
     await newInvoice.save();
     res.status(201).json({ message: "Invoice created successfully" });
   } catch (error) {
