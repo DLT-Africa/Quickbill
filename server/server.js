de8177f7c99b95e1d62f510177524d718dfd632d
yusufroqib <yusufroqib@gmail.com>
2023-12-27 16:04:16 +0100

    Tested...

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const credentials = require('./middleware/credentials');
 const initializePassport = require("./config/passport-setup")
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
@@ -15,10 +18,17 @@ const payrollRoutes = require("./routes/payrollRoutes");
 
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" })); //parse json data inside the req body
 app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
