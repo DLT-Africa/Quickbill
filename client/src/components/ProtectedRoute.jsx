323d45a6ba203dab7a5170fb155c584c2c56dbf5
yusufroqib <yusufroqib@gmail.com>
2023-12-28 23:59:07 +0100

    user atom

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {  Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
-import Login from './Login';
 
 const ProtectedRoute = ({ path, element }) => {
   const { isAuthenticated } = useAuth();
