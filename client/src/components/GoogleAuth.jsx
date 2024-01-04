be75082e8a3e1e52c255026d870df363f784797b
yusufroqib <yusufroqib@gmail.com>
2024-01-04 15:13:47 +0100

    update

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../../api/axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Flex, Spinner, Text } from "@chakra-ui/react";
