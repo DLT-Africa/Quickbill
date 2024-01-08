ce0f497d948225a67f4c0b90c4ace4c8c6a7afad
yusufroqib <yusufroqib@gmail.com>
2024-01-08 22:56:59 +0100

    Pushed client

--- client/src/components/InvSummaryItemRow.jsx
+++ client/src/components/InvSummaryItemRow.jsx
@@ -1,25 +1,18 @@
 import { Td, Text, Tr } from "@chakra-ui/react";
 import React from "react";
 
-const InvSummaryItemRow = ({row, index}) => {
+const InvSummaryItemRow = ({ row, index }) => {
 	return (
 		<Tr key={index}>
-			<Td>
+			<Td fontSize={{ base: "sm", xl: "md" }} minW={"150px"}>
 				{row.itemName}
 			</Td>
-			<Td>
-            {row.qty}
-			</Td>
-			<Td>
-            {row.price?.toFixed(2)}
-			</Td>
-			<Td>
-           {(row?.discPercent) || '--'}
-			</Td>
+			<Td fontSize={{ base: "sm", xl: "md" }}>{row.qty}</Td>
+			<Td fontSize={{ base: "sm", xl: "md" }}>{row.price?.toFixed(2)}</Td>
+			<Td fontSize={{ base: "sm", xl: "md" }}>{row?.discPercent || "--"}</Td>
 			<Td>
 				<Text>{row?.amtAfterDiscount?.toFixed(2)}</Text>
 			</Td>
-			
 		</Tr>
 	);
 };
